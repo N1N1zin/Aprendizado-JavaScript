fb284e19137128c07eccab734199bbaadb4587d1
N1N1zin <101678975+N1N1zin@users.noreply.github.com>
2022-07-07 19:04:58 -0300

    Add files via upload

---
+++ 5 - if, else/basic11.js
@@ -0,0 +1,10 @@
+var idade = 18
+if (idade < 16 ){
+     console.log('- Não vota.')
+} else {
+    if (idade <= 18){
+        console.log('- Pode votar(opcional)')
+    } else {
+        console.log('- Deve votar')
+    }
+}
