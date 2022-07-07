caa74f772abffd4da797d4b855342c7e8727f024
N1N1zin <101678975+N1N1zin@users.noreply.github.com>
2022-07-07 18:52:52 -0300

    Add files via upload

---
+++ 5 - if, else/basic8.js
@@ -0,0 +1,6 @@
+var speed = 50
+console.log(`A velocidade do seu carro é de ${speed}Km/h!`)
+if(speed > 60){
+    console.log(`Você ultrapassou da velocidade máxima permitida de 60Km/h, VOCÊ FOI MULTADO!!!`)
+}
+console.log('Use o cinto de segurança')
